c16953da79c527e5edb446f86a251062d5a4cc86
Theo Lemaire <theolemaire@MacBook-Air-de-Theo.local>
2025-02-21 12:12:26 +0100

    push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -714,7 +714,7 @@
                     {#if customText || customText2}
                         <div class="absolute top-0 left-0 w-full h-full overflow-hidden pointer-events-none">
                         <span
-                                class="absolute whitespace-nowrap"
+                                class="absolute lowercase whitespace-nowrap"
                                 style="
                                 left: {textPosition.x - (customText2 ? 1 : 0)}%;
                                 top: {textPosition.y - (customText2 ? 1 : 0)}%;
@@ -727,7 +727,7 @@
                         >{customText}</span>
                             {#if customText2}
                                 <span
-                                        class="absolute whitespace-nowrap"
+                                        class="absolute lowercase whitespace-nowrap"
                                         style="
                                     left: {textPosition.x}%;
                                     top: {textPosition.y + (customText ? 4 : 0)}%;
